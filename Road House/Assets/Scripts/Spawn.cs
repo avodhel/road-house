4a89baaed949587c618cb96b55c8f74de764c1e0
avodhel <uyelik-eposta@hotmail.com>
2020-02-05 00:15:41 +0300

    choosing path direction wisely

--- Road House/Assets/Scripts/Spawn.cs
+++ Road House/Assets/Scripts/Spawn.cs
@@ -9,9 +9,18 @@ public class Spawn : MonoBehaviour
     public GameObject normalPath;
     public GameObject[] paths;
 
+    private int randDirIndex;
+    private enum Direction
+    {
+        Top = 0,
+        Left = 1,
+        Right = 2
+    }
+    private Direction currentDir;
+
     void Start()
     {
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 100; i++)
         {
             SpawnPath();
         }
@@ -21,43 +30,67 @@ public class Spawn : MonoBehaviour
     {
         if (currentPath.tag == "mergePathTag")
         {
-            int randPosIndex = Random.Range(0, 3);
-            float rotationY = currentPath.transform.rotation.eulerAngles.y;
-            //Quaternion newRotation;
+            ChoosePathDirection();
 
-            if (randPosIndex == 0) //Top
+            float rotationY = currentPath.transform.rotation.eulerAngles.y;
+
+            if (randDirIndex == 0) //Top
             {
                 Debug.Log("Top");
-                currentPath = Instantiate(normalPath, 
-                                          currentPath.transform.GetChild(0).transform.GetChild(randPosIndex).transform.position,
+                currentDir = Direction.Top;
+                currentPath = Instantiate(normalPath,
+                                          currentPath.transform.GetChild(0).transform.GetChild(randDirIndex).transform.position,
                                           currentPath.transform.rotation);
             }
-            else if (randPosIndex == 1) //Left
+            else if (randDirIndex == 1) //Left
             {
+                currentDir = Direction.Left;
                 Debug.Log("Left");
                 var rotationVector = transform.rotation.eulerAngles;
                 rotationVector.y = rotationY - 90;
-                currentPath = Instantiate(normalPath, 
-                                          currentPath.transform.GetChild(0).transform.GetChild(randPosIndex).transform.position,
+                currentPath = Instantiate(normalPath,
+                                          currentPath.transform.GetChild(0).transform.GetChild(randDirIndex).transform.position,
                                           Quaternion.Euler(rotationVector));
             }
-            else if (randPosIndex == 2) //Right
+            else if (randDirIndex == 2) //Right
             {
+                currentDir = Direction.Right;
                 Debug.Log("Right");
                 var rotationVector = transform.rotation.eulerAngles;
                 rotationVector.y = rotationY + 90;
-                currentPath = Instantiate(normalPath, 
-                                          currentPath.transform.GetChild(0).transform.GetChild(randPosIndex).transform.position,
+                currentPath = Instantiate(normalPath,
+                                          currentPath.transform.GetChild(0).transform.GetChild(randDirIndex).transform.position,
                                           Quaternion.Euler(rotationVector));
             }
         }
         else if (currentPath.tag == "normalPathTag")
         {
-            Debug.Log("Top");
+            //Debug.Log("Top");
             currentPath = Instantiate(paths[Random.Range(0, paths.Length)], 
                                       currentPath.transform.GetChild(1).transform.position, 
                                       currentPath.transform.rotation);
         }
 
+    }
+
+    private void ChoosePathDirection()
+    {
+        //int randInt = Random.Range(0, 100);
+        if (currentDir == Direction.Left)
+        {
+            //randDirIndex = randInt < 10 ? 0 : 2;
+            randDirIndex = 2;
+        }
+        else if (currentDir == Direction.Right)
+        {
+            //randDirIndex = randInt < 10 ? 0 : 1;
+            //randDirIndex = Random.Range(0, 2);
+            randDirIndex = 1;
+        }
+        else
+        {
+            randDirIndex = Random.Range(0, 3);
+        }
+        //Debug.Log(randDirIndex);
     }
 }
