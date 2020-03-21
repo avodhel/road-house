9664589072ed09afc9c6d9bb14baa00998cf5715
avodhel <uyelik-eposta@hotmail.com>
2020-03-21 12:48:00 +0300

    transparent shader issue fixed for mobile

--- Road House/Assets/Scripts/CameraFollow.cs
+++ Road House/Assets/Scripts/CameraFollow.cs
@@ -17,35 +17,36 @@ public class CameraFollow : MonoBehaviour
     private void Update()
     {
         transform.position = car.transform.position + offset;
-        DrawRay();
+        //DrawRay();
     }
 
     private void CalculateOffsetAndDistance()
     {
         offset = transform.position - car.transform.position;
-        distance = Vector3.Distance(transform.position, car.transform.position);
+        //distance = Vector3.Distance(transform.position, car.transform.position);
     }
 
     #region Draw Raycast
 
-    private void DrawRay()
-    {
-        RaycastHit[] hits = hits = Physics.RaycastAll(transform.position,
-                                                      transform.forward,
-                                                      distance);
-        foreach (RaycastHit h in hits)
-        {
-            if (h.transform.tag == "treeTag")
-            {
-                MeshRenderer meshRend = h.transform.GetComponentInChildren<MeshRenderer>();
-                // make transparent
-                meshRend.material.shader = Shader.Find("Transparent/Diffuse");
-                Color tempColor = meshRend.material.color;
-                tempColor.a = 0.15F;
-                meshRend.material.color = tempColor;
-            }
-        }
-    }
+    //private void DrawRay()
+    //{
+    //    RaycastHit[] hits = hits = Physics.RaycastAll(transform.position,
+    //                                                  transform.forward,
+    //                                                  distance);
+    //    foreach (RaycastHit h in hits)
+    //    {
+    //        if (h.transform.tag == "treeTag")
+    //        {
+    //            MeshRenderer meshRend = h.transform.GetComponentInChildren<MeshRenderer>();
+    //            // make transparent
+    //            //meshRend.material.shader = Shader.Find("Transparent/Diffuse"); //pc
+    //            meshRend.material.shader = Shader.Find("Mobile/Transparent/Vertex Color");
+    //            Color tempColor = meshRend.material.color;
+    //            tempColor.a = 0.15F;
+    //            meshRend.material.color = tempColor;
+    //        }
+    //    }
+    //}
 
     #endregion
 }
